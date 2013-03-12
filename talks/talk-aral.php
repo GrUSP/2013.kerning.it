f8905b82ffbaa14d17da86c3b4746fdc87964c10
Matteo Balocco <matteo@matteobalocco.it>
2013-03-13 00:25:30 +0100

    Fixed link in Aral bio

--- talks/talk-aral.php
+++ talks/talk-aral.php
@@ -4,7 +4,7 @@
 		<div class="info">
 			<div class="name">Aral Balkan</div>
 			<div class="twitter"><a href="#">@aral</a></div>
-			<div class="bio"><b>Aral Balkan</b> is an experience designer and developer working to change the world through better design. He recently won Voice of the Year at the Critter Awards for his talks at international conferences and gave a talk at TED@London which has him in the running to present at TED Global 2013. He is currently rebooting. You can follow his personal reboot narrative on his hand‐crafted blog, Breaking Things.</div>
+			<div class="bio"><b>Aral Balkan</b> is an experience designer and developer working to change the world through better design. He recently won Voice of the Year at the Critter Awards for his talks at international conferences and gave a talk at TED@London which has him in the running to present at TED Global 2013. He is currently rebooting. You can follow his personal reboot narrative on his hand‐crafted blog, <a href="http://www.breakingthin.gs/">Breaking Things</a>.</div>
 		</div>
 	</div>
 	<div class="topic">
